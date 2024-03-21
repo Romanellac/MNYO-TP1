2452f6fdadf2c454dca96a06a78ef030976cca12
Romanellac <101135593+Romanellac@users.noreply.github.com>
2024-03-20 23:37:08 -0300

    Update ej1b.py

--- ej1b.py
+++ ej1b.py
@@ -21,18 +21,20 @@ def generar_puntos_equiespaciados(lim_inferior, lim_superior, cantidad_de_puntos
     return dataset
 
 
-#CHEBYSHEV con 2 variables? ***
-'''
+#CHEBYSHEV
+
 def generar_puntos_chebyshev(lim_inferior, lim_superior, cantidad_de_puntos, f):
     points = np.zeros(cantidad_de_puntos)
     for i in range(cantidad_de_puntos):
         points[i] = (lim_inferior + lim_superior) / 2 + ((lim_superior - lim_inferior) / 2) * np.cos(((2 * i + 1) * np.pi) / (2 * cantidad_de_puntos))
     
+    x1_grid, x2_grid = np.meshgrid(points, points) 
+    combinations = np.column_stack((x1_grid.flatten(), x2_grid.flatten()))
+
     dataset = {}
-    for x in points:
-        dataset[x] = f(x)
+    for combination in combinations:
+        dataset[combination] = f(combination[0], combination[1])
     return dataset
-'''
 
 
 # ---------------- ERRORES --------------   
@@ -89,7 +91,9 @@ def main():
     #Array con puntos (100) en los que vamos a testear las interpolaciones para obtener los errores:
     testing_points = generar_puntos_equiespaciados(-4,4, 1000, f1)
     x_testing = list(testing_points.keys()) #[(x11,x21), (x11, x22), ...]
+    x_testing = np.vstack(x_testing)
     y_testing = list(testing_points.values())
+    y_testing = np.vstack(y_testing)
     
     
     INFO_DICC_NN = {}
@@ -97,52 +101,54 @@ def main():
     INFO_DICC_SPL_3 = {}
     
     for i in range(3):
-        # Creamos un subplot para el gráfico actual
+        # Creamos un subplot para el gráfico actual 
         fig = plt.figure(figsize=(16, 6))
         
         # Generar puntos equiespaciados y de Chebyshev
-        dicc_equiesp = generar_puntos_equiespaciados(-4, 4, different_quantity_equiesp_points[i], f1)
-        '''
-        #dicc_chebyshev = generar_puntos_chebyshev(-4, 4, different_quantity_equiesp_points[i], f1)***
-        '''
+        dicc_equiesp = generar_puntos_equiespaciados(-4, 4, different_quantity_equiesp_points[i], f2)
+        
+        dicc_chebyshev = generar_puntos_chebyshev(-4, 4, different_quantity_equiesp_points[i], f2)
+        
 
         # Puntos conocidos por ambas funciones
-        x_equiesp = list(dicc_equiesp.keys())
+        x_equiesp = list(dicc_equiesp.keys()) # lista de numpy arrays de 1x2
+        x_equiesp = np.vstack(x_equiesp)
         y_equiesp = list(dicc_equiesp.values())
-        '''
-        #x_chebyshev = list(dicc_chebyshev.keys())
-        #y_chebyshev = list(dicc_chebyshev.values())
-        '''
+        y_equiesp = np.vstack(y_equiesp)
 
-    # Calculo los valores de la función para dichos puntos:
-        real = ''' '''
+        
+        x_chebyshev = list(dicc_chebyshev.keys())
+        x_chebyshev = np.vstack(x_chebyshev)
+        y_chebyshev = list(dicc_chebyshev.values())
+        y_chebyshev = np.vstack(y_chebyshev)
+        
 
     # Nearest Neighbor
         # Calculamos interpolación por el método del Nearest Neighbor
         NN_testing_y = NearestNeighbor(x_equiesp, y_equiesp, x_testing) 
        # Calculamos el error de aproximar con Lagrange
-        error_rel_NN = error_relativo(NN_testing_y, real)
+        error_rel_NN = error_relativo(NN_testing_y, y_testing)
 
     # Spline Cubic
         # Calculamos el polinomio con SplineCubic
         CubicSpl_testing_y = Spline_Cubic(x_equiesp, y_equiesp, x_testing)
     # Calculamos el error de aproximar con Lagrange
-        error_rel_cub = error_relativo(CubicSpl_testing_y, real)
+        error_rel_cub = error_relativo(CubicSpl_testing_y, y_testing)
 
     # Spline Cubic
         # Calculamos el polinomio con SplineCubic
         LinealSpl_testing_y = Spline_Lineal(x_equiesp, y_equiesp, x_testing)
     # Calculamos el error de aproximar con Lagrange
-        error_rel_lin = error_relativo(LinealSpl_testing_y, real)
+        error_rel_lin = error_relativo(LinealSpl_testing_y, y_testing)
 
-    '''
+    
     # Chebyshev
         # Calculamos el polinomio de Chebyshev
         Cheb_function = Chebyshev(x_chebyshev, y_chebyshev)
         chebyshev_testing_y = Cheb_function(x_testing)
        # Calculamos el error de aproximar con Lagrange
         error_rel_cheb = error_relativo(chebyshev_testing_y, y_testing)
-    '''
+    
 
     # Graficamos los errores de aproximar con el método del Nearest Neighbor
         plt.subplot(4, 1, 1)
@@ -162,13 +168,13 @@ def main():
         plt.legend()
         plt.title(f'Gráfico de error relativo al aproximar con Lineal Splines con {different_quantity_equiesp_points[i]} puntos')
 
-    '''
+    
     # Graficamos los errores de aproximar con Chebyshev
         plt.subplot(4, 1, 4)
         plt.plot(x_testing, error_rel_cheb, 'b-', label='Error Relativo')
         plt.legend()
         plt.title(f'Gráfico de error relativo al aproximar con Chebyshev con {different_quantity_equiesp_points[i]} puntos')
-    '''
+    
 
         plt.tight_layout()
         plt.show()
